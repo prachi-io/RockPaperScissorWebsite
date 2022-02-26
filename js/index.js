36d8ee0e7d41ee1d02da2f4b9e689827b8071235
Prachi2002Aggarwal <prachi2002aggarwal@gmail.com>
2022-02-27 00:12:26 +0530

    Website

--- js/index.js
+++ js/index.js
@@ -2,6 +2,8 @@
 const pickUserHand = (hand) => {
     console.log(hand);
     // hide the current page
+    let hands = document.querySelector(".hands");
+    hands.style.display = "none";
     // show the next page with hand u picked
 
 }
