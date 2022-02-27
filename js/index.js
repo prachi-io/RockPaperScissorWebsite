bde74e61cf4d0ceb7f6ae5f54246e8419640e357
Prachi2002Aggarwal <prachi2002aggarwal@gmail.com>
2022-02-27 15:47:11 +0530

    Website

--- js/index.js
+++ js/index.js
@@ -1,9 +1,24 @@
 
 const pickUserHand = (hand) => {
     console.log(hand);
+
     // hide the current page
     let hands = document.querySelector(".hands");
     hands.style.display = "none";
+
     // show the next page with hand u picked
+    let contest = document.querySelector(".contest");
+    contest.style.display = "flex";
+
+    // set the user pic
+    /*if(hand === "rock") {
+        document.getElementById("userPickImage").src = "./images/Rock.png";
+    }
+    if(hand === "scissors") {
+        document.getElementById("userPickImage").src = "./images/Scissors.png";
+    }
+    if (hand === "paper") {
+        document.getElementById("userPickImage").src = "./images/Paper.png";
+    }*/
 
 }
